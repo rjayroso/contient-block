d6099a6534c59badaae510927ffa9c7ae140d4a7
Royce Ayroso-Ong <royce.ayrosoong@gmail.com>
2021-03-29 19:49:50 -0400

    Changed data schema and added functionality to View patient data

--- index.js
+++ index.js
@@ -1,40 +1,98 @@
-const bcrypt = require('bcrypt') // import the bcrypt js librairy
+const bcrypt = require('bcrypt')
 
-class Block{ // create the block structure or class
-     
-    constructor(blockid,  previousHash, data){ // create a contractor. in a block we find this information :
-        this.blockid = blockid;  // the block id
-        this.timestamp = Date.now(); // the timestamp
-        this.blockhash = this.getHash(); // the block hash
-        this.prevHash = previousHash; // the hash of the previous block
-        this.data = data; // and all the transactions
-       
-       
+class Block {
+    constructor(blockid,  previousHash, data) {
+        this.blockid = blockid;
+        this.timestamp = Date.now();
+        this.blockhash = this.getHash();
+        this.prevHash = previousHash;
+        this.data = data;
     }
-    getHash(){
-        return bcrypt.hashSync(String(this.blockid + this.timestamp + this.blockhash + this.previousHash + JSON.stringify(this.data)) , 10) // this method will hash the data in the block using a salt of 10 and return that hash. We use the bcrypt library
+    getHash() {
+        return bcrypt.hashSync(String(this.blockid + this.timestamp + this.blockhash + this.previousHash + JSON.stringify(this.data)) , 10)
     };
 }
 
-class BlockChain{ // the blochain structure or class
-    constructor(){ // create a constractor. 
-        this.chain = []; // a blockchain is a series of blocks, so we need an array []
+class BlockChain {
+    constructor() {
+        this.chain = [];
     }
-
-    addBlock(data){ // create a method that will take the entire block and add it to the blockchain
-        let blockid = this.chain.length; // The block id will be the length or the total number of blocks in the chain minus 1, so the first block will have 0 as an index
-        let previousHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].blockhash : ''; // if it's the first block then its previous hash will be empty, if not then it will take the hash of the previous block
-        let block = new Block(blockid, previousHash, data); // Now create the block
-     
-        this.chain.push(block); // Add the block to the blockchain 
+    addBlock(data){
+        let blockid = this.chain.length;
+        let previousHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].blockhash : ''; 
+        let block = new Block(blockid, previousHash, data);
+        this.chain.push(block);
     }
-
+    viewBlock(index) {
+        console.log(JSON.stringify(this.chain[index].data.patientDetails, null, 6));
+        this.chain[index].data.history.push({dateAccessed: Date.now(), whoAccessed: "Dr. Marie Rose", action: "View"})
+    }
 }
 
-const Myfirstblockchain = new BlockChain();
+const PatientHistory = new BlockChain();
  
-Myfirstblockchain.addBlock({sender: "sinai", receiver: "kazadi", amount: 24034}); // first transaction
-Myfirstblockchain.addBlock({sender: "Dahouda", receiver: "Pat", amount: 32032}); // second transaction
-Myfirstblockchain.addBlock({sender: "Nkolomoni", receiver: "Mao", amount: 20993}); // third transaction 
- 
-console.log(JSON.stringify(Myfirstblockchain, null, 6)); // convert the result into a json and show it in the console
+PatientHistory.addBlock({
+    healthCard: "4555-152-333-XD", 
+    patientDetails: {
+        firstName: "Jane",
+        lastName: "Williams",
+        sex: "Female",
+        dateOfBirth: "01/01/1960",
+        phone: "9055553116",
+        email: "jane.williams2004@gmail.com",
+        homeAddress: "12 Heaven St. ON 1N1 2R2 ",
+        insuranceProvider: "Sunlife",
+        insurancePolicyNumber: "356M59557",
+        familyDoctorName: "Dr. Marie Rose",
+        familyDoctorPhone: "4165553324",
+        emergencyContactName: "Jake Williams",
+        emergencyContactPhone: "4164445555"
+    },
+    history: [ {dateAccessed: "19/02/2021", whoAccessed: "Dr. Marie Rose", action: "View"},
+                    {dateAccessed: "03/01/2021", whoAccessed: "Dr. Marie Rose", action: "View"},
+                    {dateAccessed: "17/10/2020", whoAccessed: "Dr. Marie Rose", action: "View"} ],
+});
+PatientHistory.addBlock({healthCard: "4555-423-332-JD",
+    patientDetails: {
+        firstName: "Martin",
+        lastName: "DeGuzman",
+        sex: "Male",
+        dateOfBirth: "12/04/1983",
+        phone: "4165553577",
+        email: "martin.deguzman@hotmail.com",
+        homeAddress: "123 Sesame St. ON 4W3 2G6",
+        insuranceProvider: "Sunlife",
+        insurancePolicyNumber: "356M545668",
+        familyDoctorName: "Dr. Jonathan Wick",
+        familyDoctorPhone: "4165552569",
+        emergencyContactName: "Jenny DeGuzman",
+        emergencyContactPhone: "4164445555"
+    },
+    history: [ {dateAccessed: "19/02/2021", whoAccessed: "Dr. Marie Rose", action: "View"}]
+});
+PatientHistory.addBlock({healthCard: "6555-362-486-XD",
+    patientDetails: {
+        firstName: "David",
+        lastName: "Cook",
+        sex: "Male",
+        dateOfBirth: "12/20/1980",
+        phone: "6478889009",
+        email: "dc_notacook@gmail.com",
+        homeAddress: "179 Co Rd 3, Fleischmanns, NY 12430, USA",
+        insuranceProvider: "Desjardins",
+        insurancePolicyNumber: "DRCKPO0099",
+        familyDoctorName: "Dr. Burg MacDonald",
+        familyDoctorPhone: "4161112424",
+        emergencyContactName: "Rachel Stump",
+        emergencyContactPhone: "9058784657"
+    },
+    history: [ {dateAccessed: "19/02/2021", whoAccessed: "Dr. Marie Rose", action: "View"}],
+});
+
+// Demo
+console.log("====== DISPLAYING THE ENTIRE BLOCKCHAIN =============");
+console.log(JSON.stringify(PatientHistory, null, 6));
+console.log("====== VIEWING JANE'S PATIENT DATA ==================");
+PatientHistory.viewBlock(0);
+console.log("====== VIEWING THE UPDATED TRANSACTION HISTORY ======");
+console.log(JSON.stringify(PatientHistory, null, 6));
